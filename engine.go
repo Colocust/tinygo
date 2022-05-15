200e7914dbb843a783e741c33b037c20bbf54643
Colocust <516100080@qq.com>
2022-05-15 19:49:43 +0800

    完成404处理

--- engine.go
+++ engine.go
@@ -1,6 +1,9 @@
 package tinygo
 
-import "sync"
+import (
+	"net/http"
+	"sync"
+)
 
 type Engine struct {
 	RouterGroup               // 初始路由组
@@ -16,6 +19,10 @@ type HandlerFunc func(ctx *Context)
 
 type HandlersChain []HandlerFunc
 
+var (
+	default404Body = []byte("404 page not found")
+)
+
 func New() *Engine {
 	engine := &Engine{
 		RouterGroup: RouterGroup{
@@ -27,6 +34,9 @@ func New() *Engine {
 	}
 
 	engine.RouterGroup.engine = engine
+	engine.pool.New = func() interface{} {
+		return engine.allocateContext()
+	}
 
 	return engine
 }
@@ -44,6 +54,44 @@ func (engine *Engine) Use(handlers ...HandlerFunc) Routes {
 	return engine
 }
 
+func (engine *Engine) Run(addr string) (err error) {
+	err = http.ListenAndServe(addr, engine)
+	return
+}
+
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	ctx := engine.pool.Get().(*Context)
+	ctx.Writer.reset(w)
+	ctx.Reset()
+	ctx.Request = req
+
+	engine.handleHTTPRequest(ctx)
+	engine.pool.Put(ctx)
+}
+
+func (engine *Engine) handleHTTPRequest(ctx *Context) {
+	method, _, trees := ctx.Request.Method, ctx.Request.URL.Path, engine.trees
+	for i, tl := 0, len(trees); i < tl; i++ {
+		if method != trees[i].method {
+			continue
+		}
+		//root := trees[i].root
+		//value := root.getValue(path)
+	}
+
+	ctx.handlers = engine.allNoRoute
+	serveError(ctx, http.StatusNotFound, default404Body)
+}
+
+func serveError(ctx *Context, status int, defaultMessage []byte) {
+	ctx.Writer.status = status
+	ctx.Next()
+	if ctx.Writer.Written() {
+		return
+	}
+	ctx.Writer.Write(defaultMessage)
+}
+
 func (engine *Engine) NoRoute(handlers ...HandlerFunc) {
 	engine.noRoute = handlers
 	engine.rebuild404Handlers()
@@ -51,6 +99,11 @@ func (engine *Engine) NoRoute(handlers ...HandlerFunc) {
 
 func (engine *Engine) NoMethod(handlers ...HandlerFunc) {
 	engine.noMethod = handlers
+}
+
+// 申请context资源
+func (engine *Engine) allocateContext() *Context {
+	return &Context{engine: engine, Writer: &ResponseWriter{}}
 }
 
 func (engine *Engine) rebuild404Handlers() {
